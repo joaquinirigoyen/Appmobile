1db15f26c8444fd1c9ad0dc6dc55310e3d2e28a6
joaquin irigoyen <joaquin.irigoyen.epet20@gmail.com>
2021-05-17 01:27:06 -0300

    agregando el boton flotante

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("mi primera app")), //appbar
-          body: Container(
-              child: Center(
-            child: Text(
-              "hello word",
-              style: TextStyle(fontSize: 40),
-            ),
-          ))));
+    appBar: AppBar(title: Text("mi primera app")), //appbar
+    body: Container(
+        child: Center(
+      child: Text(
+        "hello word",
+        style: TextStyle(fontSize: 40),
+      ),
+    )),
+    floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print("hola");
+        }),
+  ));
 
   runApp(app);
 }
