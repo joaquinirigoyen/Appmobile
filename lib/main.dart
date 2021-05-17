3f29f70d0e580d65114556aecd5d8f675923f3c9
joaquin irigoyen <joaquin.irigoyen.epet20@gmail.com>
2021-05-17 16:53:37 -0300

    agregando los botones flotones

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,68 @@ import 'package:flutter/material.dart';
 
 void main() {
   var app = MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(title: Text("mi primera app")), //appbar
-    body: Container(
-        child: Center(
-      child: Text(
-        "hello word",
-        style: TextStyle(fontSize: 40),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("mi primera app"),
+        ),
       ),
-    )),
-    floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print("hola");
-        }),
-  ));
+      body: Container(
+        child: Center(
+          child: Text(
+            "hello word",
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Botonflotante(),
+      bottomNavigationBar: NavegacionBoton(),
+    ),
+  );
 
   runApp(app);
+}
+
+class NavegacionBoton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: [
+        // ignore: deprecated_member_use
+        BottomNavigationBarItem(
+            // ignore: deprecated_member_use
+            icon: Icon(Icons.access_alarm_sharp),
+            title: Text("Alarma")),
+        BottomNavigationBarItem(
+            // ignore: deprecated_member_use
+            icon: Icon(Icons.ac_unit_outlined),
+            title: Text("hielo")),
+      ],
+    );
+  }
+}
+
+class Botonflotante extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+        child: Icon(Icons.search),
+        onPressed: () {
+          // print("no tiene uso todavia");
+          Navigator.push(context,
+              MaterialPageRoute(builder: (BuildContext context) => Pagina2()));
+        });
+  }
+}
+
+class Pagina2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButton: Botonflotante(),
+      bottomNavigationBar: NavegacionBoton(),
+    );
+  }
 }
