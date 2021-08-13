47a8ee5b75aeb26c997e748c7871bc8e6b795935
joaquin irigoyen <joaquin.irigoyen.epet20@gmail.com>
2021-08-13 09:23:47 -0300

    agregando lo que hizo benja

---
+++ lib/src/gimnasio.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class gimnasio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Gimnasio EPET 20"),
+        ),
+      ),
+    );
+  }
+}
