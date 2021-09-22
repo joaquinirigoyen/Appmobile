8e53379ad577264af82ad53b4991e72512b7cb80
Benjamin Quiroga <benjamin.quiroga.epet20@gmail.com>
2021-09-22 01:49:21 -0300

    agregue el primer patron bloc

--- lib/BLoc/bloc.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:async';
-import 'dart:html';
-import 'package:BLoc/State/state.dart';
-import 'package:BLoc/Event/event.dart';
-
-class HomesBloc extends Bloc<HomesEvent, HomesState> {}
